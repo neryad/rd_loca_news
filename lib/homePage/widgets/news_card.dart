c48ffcf6ede7efe7aaee52e930452a70e8b5e2f8
Neryad <dayern.gomez@hotmail.com>
2024-07-10 21:00:43 -0400

    fix on list

--- lib/homePage/widgets/news_card.dart
+++ lib/homePage/widgets/news_card.dart
@@ -23,70 +23,71 @@ class NewsCard extends StatelessWidget {
         final List<News> news = snapshot.data;
 
         return Padding(
-          padding: const EdgeInsets.all(8.0),
+          padding: const EdgeInsets.all(5.0),
           child: ListView.builder(
               itemCount: news.length,
               itemBuilder: (context, index) {
-                return Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Row(
-                    children: [
-                      ClipRRect(
-                        // width: 150,
-                        // height: 150,
-                        borderRadius: BorderRadius.circular(10.0),
-                        child: Image.network(
-                          news[index].img,
-                          width: 100,
-                          height: 100,
-                          fit: BoxFit.cover,
-                        ),
+                return Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(10.0),
+                      child: Image.network(
+                        news[index].img,
+                        width: 150,
+                        height: 120,
+                        fit: BoxFit.cover,
                       ),
-                      Padding(
-                        padding: const EdgeInsets.all(10.0),
-                        child: Column(
-                          // mainAxisAlignment: MainAxisAlignment.center,
-                          // crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            SizedBox(
-                                width: 250,
-                                child: Text(
-                                  news[index].title,
-                                )),
-                            SizedBox(
-                              child: Row(
-                                children: [
-                                  TextButton(
-                                      onPressed: () {
-                                        Navigator.push(
-                                            context,
-                                            MaterialPageRoute(
-                                                builder: (context) =>
-                                                    WebViewPage(
-                                                      newsUrl: news[index].url,
-                                                    )));
-                                      },
-                                      child: const Text('Leer mas')),
-                                  IconButton(
-                                      onPressed: () {},
-                                      icon: const Icon(Icons.bookmark)),
-                                  IconButton(
-                                      onPressed: () {
-                                        final messageToShare =
-                                            '📰 ¡Mantente al día con nuestra nueva app de noticias! 📱\n👉 ${news[index].url} \nNoticias frescas y análisis interesantes, ¡directo en tu bolsillo! 🌟';
-                                        Share.share(messageToShare);
-                                        // Share.shareUri(Uri.parse(
-                                        //     'mira esta notiocia:$news[index].url'));
-                                      },
-                                      icon: const Icon(Icons.share))
-                                ],
-                              ),
-                            )
-                          ],
-                        ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Column(
+                        // mainAxisAlignment: MainAxisAlignment.center,
+                        // crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          SizedBox(
+                              width: MediaQuery.of(context).size.width / 2,
+                              child: Text(
+                                news[index].title,
+                                style: const TextStyle(
+                                    overflow: TextOverflow.clip,
+                                    fontSize: 17,
+                                    fontWeight: FontWeight.bold),
+                              )),
+                          SizedBox(
+                            child: Row(
+                              children: [
+                                TextButton(
+                                    onPressed: () {
+                                      Navigator.push(
+                                          context,
+                                          MaterialPageRoute(
+                                              builder: (context) => WebViewPage(
+                                                    newsUrl: news[index].url,
+                                                  )));
+                                    },
+                                    child: const Text('Leer mas')),
+                                IconButton(
+                                    onPressed: () {},
+                                    icon: const Icon(Icons.bookmark)),
+                                IconButton(
+                                    onPressed: () {
+                                      final messageToShare =
+                                          '¡Últimas noticias: ${news[index].title}! 📰\n 📰 ¡Mantente al día con nuestra nueva app de noticias! 📱\n👉 ${news[index].url}\n\n¡Descarga NeryNews ya y no te pierdas ninguna noticia! 🚀📲';
+
+                                      Share.share(
+                                        messageToShare,
+                                      );
+                                    },
+                                    icon: const Icon(Icons.share))
+                              ],
+                            ),
+                          )
+                        ],
                       ),
-                    ],
-                  ),
+                    ),
+                  ],
                 );
                 //  return newsTile(news[index], context);
               }),
